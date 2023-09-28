e5f300bdae09725bef7c32ffaaa04c7408845cd0
Aberejo Habeeblah O. <habeeblahaberejo01@gmail.com>
2023-09-28 14:43:41 +0100

    send button issues resolved

--- README.py
+++
@@ -1,6 +0,0 @@
-A README
-# Just my portfolio
-## 💡My simple website with my portfolio with nice features ::) 
-**Adding more content to this later!!**
-
-## 👩‍💻 Checkout live page - https://bheez.netlify.app:)
